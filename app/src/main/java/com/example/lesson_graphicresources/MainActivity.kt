f0096d3f0dbff9b826e6f86dda83bce7c8236d2e
docfr10 <docfr10@yandex.ru>
2023-11-17 13:23:33 +0300

    Topic is completed

--- app/src/main/java/com/example/lesson_graphicresources/MainActivity.kt
+++ app/src/main/java/com/example/lesson_graphicresources/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.lesson_graphicresources
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        val imageView: ImageView = findViewById(R.id.imageView)
+        imageView.visibility = View.INVISIBLE
+
+        val textView: TextView = findViewById(R.id.textView)
+
+        textView.setOnClickListener {
+            imageView.visibility = View.VISIBLE
+            imageView.setImageResource(R.mipmap.ic_launcher)
+            val animation1 = AnimationUtils.loadAnimation(applicationContext, R.anim.animation2)
+            imageView.startAnimation(animation1)
+        }
     }
 }
